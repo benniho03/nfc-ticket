ddd94d728b630233b4fca3db157d83c5299ccd59
on21FU <94552370+on21FU@users.noreply.github.com>
2023-12-15 15:31:52 +0100

    added validation for tickets

---
+++ src/pages/api/tickets.ts
@@ -0,0 +1,75 @@
+import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+
+type ResponseData = SuccessfulValidation | InvalidUserInput | WrongHttpReq | NoTicketFound
+
+type SuccessfulValidation = {
+    message: string,
+    name: string
+}
+
+type InvalidUserInput = {
+    message: string;
+    expected: Record<string, string>,
+}
+
+type WrongHttpReq = {
+    message: string;
+    expected: 'POST'
+}
+
+type NoTicketFound = {
+    message: string;
+}
+
+
+type TicketInfo = z.infer<typeof ticketInfoSchema>
+
+const ticketInfoSchema = z.object({
+    ticketId: z.string(),
+    eventId: z.string()
+})
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+    if (req.method !== 'POST') { 
+        res.status(405).json({ message: 'Wrong HTTP-method', expected: 'POST' })
+        return
+    }
+
+    if(!checkUserInput(req.body)){
+        res.status(400).json({ message: 'Invalid format', expected: { tickedId: 'String', eventId: 'String' }})
+        return
+    }
+
+    const {ticketId, eventId} = req.body;
+
+    const ticket = await getTicketInfo(ticketId, eventId)
+    if(!ticket){
+        res.status(400).json({ message: 'Ticket doesnt exsits' });
+        return
+    }
+
+    res.status(200).json({ message: 'Valid ticket', name: "Name noch nicht hinterlegt" })
+}
+
+function checkUserInput(userInput: unknown): userInput is TicketInfo {
+    return ticketInfoSchema.safeParse(userInput).success;    
+}
+
+async function getTicketInfo(ticketId: string, eventId: string){
+    const prisma = new PrismaClient();
+    return await prisma.ticket.findFirst({
+        where: {
+            id: {
+                equals: ticketId
+            },
+            eventId: {
+                equals: eventId
+            }
+        }
+    });
+}
