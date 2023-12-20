f78f9bb372d770c140e039083863fc4a5d04d33c
paulhnn26 <pau.henn.21@lehre.mosbach.dhbw.de>
2023-12-20 10:09:48 +0100

    Design Event-Slider

--- src/components/event-slider.tsx
+++ src/components/event-slider.tsx
@@ -7,10 +7,16 @@ export const EventSlider = ({ events }: { events: EventDetails[] }) => {
   const [emblaRef] = useEmblaCarousel( { loop: true, }, [Autoplay({delay : 10000})]);
 
   return (
-    <div className="embla  bg-slate-500 " ref={emblaRef}>
+    <div className="embla  bg-slate-200 " ref={emblaRef}>
       <div className="embla__container">
         {events.map((event) => {
-          return <div className="embla__slide">{event.name}</div>;
+          return <div className="embla__slide p-5 ">
+            <div className="text-2xl ">{event.name}</div>
+            <div className="text-lg ">{event.date.toLocaleDateString()}</div>
+            <div className="text-lg ">{event.location}</div>
+            <div className="text-lg ">Ein BILD FEHLT</div>
+            
+            </div>;
         })}
       </div>
     </div>
