9093c93bb04cfe667cd2c8ec14fb9ac6296d0471
on21FU <94552370+on21FU@users.noreply.github.com>
2023-12-27 16:47:23 +0100

    add basic informations

--- src/pages/event/[eventId]/admin/index.tsx
+++ src/pages/event/[eventId]/admin/index.tsx
@@ -1,4 +1,23 @@
+import { api } from "@/utils/api"
+import { useRouter } from "next/router"
 
 export default function EventDashboard(){
-    return ("Hello world")
+    const router = useRouter()
+    const eventId = router.query.eventId as string
+    const { data: event, isLoading, error } = api.event.getOne.useQuery({ eventId })
+
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>An Error Occured</div>
+    if (!event) return <div>404</div>
+
+    return (
+        <>
+            <h1>Admin Dashboard</h1>
+            <h2>{event.name}, {event.date.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric'})}</h2>
+            <h2>{event.location}, {event.locationAdress}</h2>
+
+
+            <p>Generierter Umsatz: {event.ticketsSold * event.ticketPrice} €</p>
+        </>
+    )
 }
