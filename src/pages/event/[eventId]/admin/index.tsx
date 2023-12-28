387410b62e9ee849708efafc0f9b3b50a8817c7b
on21FU <94552370+on21FU@users.noreply.github.com>
2023-12-28 13:53:32 +0100

    added pie charts, database request not working

--- src/pages/event/[eventId]/admin/index.tsx
+++ src/pages/event/[eventId]/admin/index.tsx
@@ -1,23 +1,38 @@
+import PieChart from "@/components/piechart"
 import { api } from "@/utils/api"
+import { PrismaClient } from "@prisma/client"
 import { useRouter } from "next/router"
 
-export default function EventDashboard(){
+
+
+export default async function EventDashboard(){
     const router = useRouter()
     const eventId = router.query.eventId as string
     const { data: event, isLoading, error } = api.event.getOne.useQuery({ eventId })
-
+    
     if (isLoading) return <div>Loading...</div>
     if (error) return <div>An Error Occured</div>
     if (!event) return <div>404</div>
-
+    
     return (
         <>
             <h1>Admin Dashboard</h1>
             <h2>{event.name}, {event.date.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric'})}</h2>
             <h2>{event.location}, {event.locationAdress}</h2>
-
+            <PieChart label1="Verkaufte Tickets" label2="Unverkaufte Tickets" value1={event.ticketsSold} value2={event.maxTicketAmount - event.ticketsSold} />
+            <PieChart label1="Eingecheckt" label2="Verkaufte Tickets" value1={await getCheckedIn(eventId)} value2={event.ticketsSold} />
 
             <p>Generierter Umsatz: {event.ticketsSold * event.ticketPrice} €</p>
         </>
     )
+}
+
+async function getCheckedIn(eventId: string){
+    const prisma = new PrismaClient();
+    return await prisma.ticket.count({
+        where: {
+            eventId: eventId,
+            status: "CLOSED"
+        }
+    })
 }
