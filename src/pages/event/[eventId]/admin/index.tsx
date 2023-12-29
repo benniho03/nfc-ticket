c19ae796691d5113f443a7e393f8236ca7676561
on21FU <94552370+on21FU@users.noreply.github.com>
2023-12-29 11:18:28 +0100

    one request for count closed tickets and count open tickets

--- src/pages/event/[eventId]/admin/index.tsx
+++ src/pages/event/[eventId]/admin/index.tsx
@@ -8,16 +8,22 @@ import { useRouter } from "next/router"
 export async function getServerSideProps(context: GetServerSidePropsContext){
     const eventId = context.query.eventId as string
     const prisma = new PrismaClient();
-    const res = await prisma.ticket.count({
+    const cCount = await prisma.ticket.count({
         where: {
             eventId: eventId,
             status: "CLOSED"
         }
     })
-    return { props: { closedCount: res } }
+    const oCount = await prisma.ticket.count({
+        where: {
+            eventId: eventId,
+            status: "OPEN"
+        }
+    })
+    return { props: { closedCount: cCount, openCount: oCount } }
 }
 
-export default function EventDashboard({closedCount}: InferGetServerSidePropsType<typeof getServerSideProps>){
+export default function EventDashboard({closedCount, openCount}: InferGetServerSidePropsType<typeof getServerSideProps>){
     const router = useRouter()
     const eventId = router.query.eventId as string
     const { data: event, isLoading, error } = api.event.getOne.useQuery({ eventId })
@@ -29,12 +35,14 @@ export default function EventDashboard({closedCount}: InferGetServerSidePropsTyp
     return (
         <>
             <h1>Admin Dashboard</h1>
-            <h2>{event.name}, {event.date.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric'})}</h2>
-            <h2>{event.location}, {event.locationAdress}</h2>
-            <PieChart label1="Verkaufte Tickets" label2="Unverkaufte Tickets" value1={event.ticketsSold} value2={event.maxTicketAmount - event.ticketsSold} />
-            <PieChart label1="Eingecheckt" label2="Noch nicht eingecheckt" value1={closedCount} value2={event.ticketsSold-closedCount} />
+            <h2>{ event.name }, { event.date.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' }) }</h2>
+            <h2>{ event.location }, { event.locationAdress }</h2>
+            <div style={{ width: '250px' }}>
+                <PieChart label1="Verkaufte Tickets" label2="Unverkaufte Tickets" value1={ event.ticketsSold } value2={ event.maxTicketAmount - event.ticketsSold } />
+                <PieChart label1="Eingecheckt" label2="Noch nicht eingecheckt" value1={ closedCount } value2={ openCount } />
+            </div>
 
-            <p>Generierter Umsatz: {event.ticketsSold * event.ticketPrice} €</p>
+            <p>Generierter Umsatz: { event.ticketsSold * event.ticketPrice } €</p>
         </>
     )
 }
